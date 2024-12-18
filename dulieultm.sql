b21a0b0f3a2868eb92df12b912392fc2b176198d
ngoquocanh20022004 <167946192+ngoquocanh20022004@users.noreply.github.com>
2024-12-18 23:31:22 +0700

    Add files via upload

---
+++ dulieultm.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th12 16, 2024 lúc 09:29 AM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `dulieultm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `account`
+--
+
+CREATE TABLE `account` (
+  `ID` int(11) NOT NULL,
+  `Name` text NOT NULL,
+  `Username` text NOT NULL,
+  `Password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `account`
+--
+
+INSERT INTO `account` (`ID`, `Name`, `Username`, `Password`) VALUES
+(1, 'Ngo Quoc Anh', 'user1', '1234'),
+(2, 'Hoang Duc An', 'user2', '1234'),
+(3, 'An Hoang', 'user3', '1234'),
+(4, 'Anh Ngo', 'user4', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `file`
+--
+
+CREATE TABLE `file` (
+  `IDFile` int(11) NOT NULL,
+  `NameFile` text NOT NULL,
+  `UserID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Chỉ mục cho bảng `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`IDFile`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `file`
+--
+ALTER TABLE `file`
+  MODIFY `IDFile` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
